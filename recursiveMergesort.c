2df3360a76681bd8ea716198e0ae71a5ba8abebb
GianlucaVico <ganlucavico@gmail.com>
2022-11-03 20:48:55 +0100

    Serial merge sort

---
+++ recursiveMergesort.c
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+#include "recursiveMergesort.h"
+/*
+Merge 2 sorted subarray in a sorted way
+
+Args:
+    int *p: input array containing the 2 sub / output the sorted array
+    int start: starting index of first subarray
+    int mid: starting index of the second subarray
+    int end: ending position of the second subarray (not included in the array)
+*/
+void merge(int *p, int first, int second, int end) {
+    int* tmp = malloc(sizeof(int) * (second - first));
+    int i, j, k;  // Iterators for the subarrays and the array
+    int l1 = second - first; // Lengths of the arrays
+    int l2 = end - second + 1;
+    // Move the first subarray
+    for(i = 0; i < l1; i++) {
+        tmp[i] = p[first + i];
+    }
+
+    // Store the result in p
+    i = 0;
+    j = second;
+    k = first;
+    while((i < l1) && (j < end)) {
+        if(tmp[i] < p[j]) {
+            p[k] = tmp[i];
+            i++;
+
+        } else {
+            p[k] = p[j];
+            j++;
+        }
+        k++;
+    }
+
+    while(i < l1) {
+        p[k] = tmp[i];
+        i++;
+        k++;
+    }
+
+    // Probably not needed: the second subarray is already sorted and in p
+    // while(j < end) {
+    //     p[k] = p[j];
+    //     j++;
+    //     k++;
+    // }
+    free(tmp);
+};
+
+/*
+Serial recursive implementation of Merge Sort
+
+Args:
+    int *p: input array to be sorted / output sorted array
+    int start: starting index of the array to sort
+    int end: last index of the array to sort
+*/
+void recursiveMergesort(int *p, int start, int end) {   
+    if((end - start) <= 1) {  // TODO: use diff. sort algo if end - start < threshold
+        return;
+    } else {
+        int mid = (start + end) / 2;
+        recursiveMergesort(p, start, mid);
+        recursiveMergesort(p, mid, end);
+        merge(p, start, mid, end);
+    }
+};
+
---
+++ test_recursiveMergesort.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "recursiveMergesort.h"
+
+void printIntArray(int *p, int len) {
+    int i;
+    for(i = 0; i < len; i++) {
+        printf("%d ", p[i]);
+    }
+    printf("\n");
+}
+
+void testMerge(int *p, int len) {
+    printf("-----------------\n");
+    printf("Array: ");
+    printIntArray(p, len);
+    merge(p, 0, len/2, len);
+    printf("Merged: ");
+    printIntArray(p, len);
+    printf("-----------------\n");
+}
+
+void testSort(int *p, int len) {
+    printf("-----------------\n");
+    printf("Array: ");
+    printIntArray(p, len);
+    recursiveMergesort(p, 0, len);
+    printf("Sorted: ");
+    printIntArray(p, len);
+    printf("-----------------\n");
+}
+
+int main(int argc, char const *argv[])
+{   
+    const int L = 10;
+    printf("#############\n");
+    printf("Merge part\n");
+    int m1[10] = {1,2,3,4,5,6,7,8,9,10};
+    int m2[10] = {6,7,8,9,10,1,2,3,4,5};
+    int m3[10] = {1,3,5,7,9,2,4,6,8,10};
+    int m4[10] = {1,3,5,7,9,2,4,6,8};
+    
+    testMerge(m1, L);
+    testMerge(m2, L);
+    testMerge(m3, L);
+    testMerge(m4, L-1);
+
+    printf("#############\n");
+    printf("Sort part\n");
+    int s1[10] = {10,9,8,7,6,5,4,3,2,1};
+    int s2[10] = {10,4,3,7,6,1,2,8,5,9};
+    int s3[10] = {4,2,2,4};
+    testSort(m1, L);
+    testSort(m4, L - 1);
+    testSort(s1, L);
+    testSort(s2, L);
+    testSort(s3, 4);
+
+    return 0;
+}
